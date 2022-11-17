d08a225ec49eb42aa243d2b4b3ac9d16d8fe6019
Martin Unhjem <>
2022-11-17 15:16:31 +0100

    Laget beskrivelse for hvordan komme i gang med eiendom og revisors signering

--- docs/test/testinnsending/testTid.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timezone, timedelta
-
-visible_after = datetime.now(timezone.utc)
-due_before = visible_after + timedelta(days=30)
-
-visible_after_iso_date = visible_after.astimezone().isoformat()
-due_before_iso_format = due_before.astimezone().isoformat()
-print('visiable', visiable_before_iso_date)
-print('due_date', due_date_iso_format)
